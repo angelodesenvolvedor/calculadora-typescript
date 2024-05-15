0949309cf71d23257d1b9a07244af33beeaebb0f
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2024-05-15 09:30:23 -0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,9 @@
+function soma(a: number, b: number): number {
+    return a + b;
+}
+
+const num1: number = 5;
+const num2: number = 3;
+const resultado: number = soma(num1, num2);
+
+console.log(`A soma de ${num1} e ${num2} é ${resultado}.`);
